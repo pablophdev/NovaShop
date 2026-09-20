62c1c5c2d9048b1b4e3abadfbe698aa61ea8b758
pablophdev <pablo.philimon.zuniga@gmail.com>
2026-09-20 15:50:40 -0300

    feat: se modifica application.properties y ademas se crea CustomerRepository

---
+++ customer-service/src/main/java/com/pabloph/customer_service/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.pabloph.customer_service.repository;
+
+import com.pabloph.customer_service.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
